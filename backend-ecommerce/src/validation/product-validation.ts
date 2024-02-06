5b509f5285c418149ac02dc504596053e53e8cdd
Manas Kumar Jena <pixeldesignindia2023@gmail.com>
2024-02-06 17:58:08 +0530

    photos zod validation

--- backend-ecommerce/src/validation/product-validation.ts
+++ backend-ecommerce/src/validation/product-validation.ts
@@ -31,9 +31,7 @@ export const productSchema = z.object({
     invalid_type_error: "stock should be number"})
   .min(0, { message: "Stock is required" }),
 
-  photo: z
-  .string()
-  .min(1, { message: "Photo is required" }),
+  photos: z.array(z.string()).min(1, "At least one photo is required"),
    discription: z
     .string({
       required_error: 'Product Name is required',
