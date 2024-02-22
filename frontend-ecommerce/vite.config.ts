d2fe2edb080847b058617b8f23d9d9dbe5e6131c
Bapi Majumder <122961065+BapiMajumder1402@users.noreply.github.com>
2024-02-22 11:40:26 +0530

    config

--- frontend-ecommerce/vite.config.ts
+++ frontend-ecommerce/vite.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite'
+import path from 'path'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // base:'/vite-website/',
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   server:{
     port: 3000,
     open: true,
